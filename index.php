3e98a7c5af60ed0705effb41eccfd0b2b4fe4730
Longnt <longntph18948@fpt.edu.vn>
2022-01-21 22:32:07 +0700

    add menu

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<nav class="header-menu">
+    <ul class="header-menu-list">
+        <li class="header-menu-item">
+            <a href="./index.html" class="header-menu-link active">Trang chủ</a>
+        </li>
+
+        <li class="header-menu-item">
+            <a href="./pages/juice/juice.html" class="header-menu-link">Nước ép</a>
+        </li>
+
+        <li class="header-menu-item">
+            <a href="./pages/fruits/fruits.html" class="header-menu-link">Trái cây</a>
+        </li>
+
+        <li class="header-menu-item">
+            <a href="#" class="header-menu-link">Tin tức</a>
+        </li>
+
+        <li class="header-menu-item">
+            <a href="#" class="header-menu-link">Liên hệ</a>
+        </li>
+
+        <li class="header-menu-item" id="header-menu-close">
+            <a href="#" class="header-menu-link">
+                <i class="fas fa-times"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
 <?php
 echo "hello!";
 ?>
